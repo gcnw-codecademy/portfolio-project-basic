5c26289cad9bc82f52246c583fead5ac179e4d79
gcnw <SkillCatcher@pm.me>
2022-05-07 19:14:33 -0700

    updated with new projects

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,4 @@
-const projectElements = ['projectOne','projectTwo',/*'projectThree'*/,'projectFour','projectFive','projectSix','projectSeven','projectEight', 'projectNine', 'projectTen'];
+const projectElements = [/*'projectOne'*/,'projectTwo',/*'projectThree'*/,'projectFour','projectFive','projectSix','projectSeven','projectEight', 'projectNine', 'projectTen','projectEleven','projectTwelve'];
 const skillElements = ['skillCSS','skillHTML','skillJS'];
 const contactElements = ['emailWidget','discordWidget','githubWidget'];
 const navElements = ['aboutNAV','projectNAV','certsNAV','contactNAV','futureNAV'];
